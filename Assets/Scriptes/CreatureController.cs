bae4b82e7f1626ac349bb5e9a381d5151b8cbaf2
Damien Rouzeau <86470640+DamienRouzeau@users.noreply.github.com>
2022-11-15 12:34:42 +0100

    creature

--- Assets/Scriptes/CreatureController.cs
+++ Assets/Scriptes/CreatureController.cs
@@ -9,11 +9,18 @@ public class CreatureController : MonoBehaviour
     public Animator _Animator;
     public AudioSource _ISeeYou;
 
+
+
+    private bool _wanderOnLeft, _wanderOnRight;
+    private float _sideForBegginWandering = 0f;
+    [SerializeField] Vector2 positionforarea;
     private SpriteRenderer _sprite;
-    private bool _IsTrigered = false;
-    [SerializeField] float _Speed = 6f;
+    [SerializeField] float _speed;
+    private bool _isWandering;
+    [SerializeField] float _SpeedWhenTriggered = 6f;
+    [SerializeField] float _SpeedWhenNotTriggered = 4f;
     [SerializeField] Rigidbody2D rb;
-    // Start is called before the first frame update
+
     void Start()
     {
         _Animator = GetComponent<Animator>();
@@ -21,40 +28,37 @@ public class CreatureController : MonoBehaviour
         _player = FindObjectOfType<PlayerController>();
         _sprite = GetComponent<SpriteRenderer>();
         _ISeeYou = GetComponent<AudioSource>();
+        positionforarea = rb.position;
+        StartCoroutine(Wander());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
-        if (_IsTrigered)
+        if (!_isWandering)
         {
-            if (this.transform.position.x - _player.transform.position.x < 0)
-            {
-                rb.MovePosition(rb.position + Vector2.right * _Speed * Time.deltaTime);
-                _sprite.flipX = true;
-            }
-            else
-            {
-                rb.MovePosition(rb.position + Vector2.left * _Speed * Time.deltaTime);
-                _sprite.flipX = false;
-            }
+            StartCoroutine(Wander());
         }
-        else
-        {
-            rb.MovePosition(rb.position);
-        }
+        rb.MovePosition(rb.position + Vector2.right * _speed * Time.deltaTime);
     }
 
     public void OnTriggerEnter2D(Collider2D area)
     {
+
         if (area.CompareTag("Player"))
         {
-            _IsTrigered = true;
+            StopCoroutine(Wander());
+            
+            if (this.transform.position.x - _player.transform.position.x < 0)
+            {
+                _speed = _SpeedWhenTriggered;
+                _sprite.flipX = true;
+            }
+            else
+            {
+                _speed = -_SpeedWhenTriggered;
+                _sprite.flipX = false;
+            }
             _Animator.SetBool("Walk", true);
             _ISeeYou.Play();
         }
@@ -63,12 +67,64 @@ public class CreatureController : MonoBehaviour
     {
         if (area.CompareTag("Player"))
         {
-            _IsTrigered = false;
+            _speed = 0f;
             _Animator.SetBool("Walk", false);
             _ISeeYou.Stop();
-
+            StartCoroutine(NewWanderingArea());
         }
     }
 
+    public IEnumerator NewWanderingArea()
+    {
+        yield return new WaitForSeconds(4f);
+        positionforarea = rb.position;
 
+        StartCoroutine(Wander());
+    }
+
+    public IEnumerator Wander()
+    {
+        _isWandering = true;
+        //determine dans quel sens va la créature
+        _sideForBegginWandering = Random.Range(-0.1f, 0.1f);
+        rb.MovePosition(rb.position + Vector2.left * _sideForBegginWandering);
+        if (rb.position.x < positionforarea.x)
+        {
+            _wanderOnLeft = true;
+        }
+        else if (rb.position.x > positionforarea.x)
+        {
+            _wanderOnRight = true;
+        }
+
+        if (_wanderOnLeft)
+        {
+            if (rb.position.x < positionforarea.x - 5f)
+            {
+                _speed = _SpeedWhenNotTriggered;
+                _sprite.flipX = false;
+            }
+            else
+            {
+                _speed = 0f;
+                yield return new WaitForSeconds(3f);
+                _wanderOnLeft = false;
+            }
+        }
+        else if (_wanderOnRight)
+        {
+            if (rb.position.x > positionforarea.x + 5f)
+            {
+                _speed = -_SpeedWhenNotTriggered;
+                _sprite.flipX = true;
+            }
+            else
+            {
+                _speed = 0f;
+                yield return new WaitForSeconds(3f);
+                _wanderOnRight = false;
+            }
+        }
+        _isWandering = false;
+    }
 }
