ae4ff2655b25c35a14f2226606ecb822ede85c10
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2022-11-15 12:33:20 +0100

    add menu

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour
+{
+    public GameObject selectOption;
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(selectOption);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
